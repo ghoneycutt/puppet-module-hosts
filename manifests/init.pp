39df2f155f9c59959054c86e892487361a6b7168
Garrett Honeycutt <code@garretthoneycutt.com>
2018-05-03 11:57:25 -0400

    Stop managing localhost entries

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,59 +2,24 @@
 #
 # Manage /etc/hosts
 #
-# @param enable_ipv4_localhost Boolean to enable ipv4 localhost entry.
-# @param enable_ipv6_localhost Boolean to enable ipv6 localhost entry.
-# @param enable_fqdn_entry Boolean to enable entry for fqdn.
-# @param fqdn_host_aliases String or Array of aliases for fqdn.
-# @param localhost_aliases Array of aliases for localhost.
-# @param localhost6_aliases Array of aliases for localhost6.
-# @param purge_hosts Boolean to optionally purge unmanaged entries from hosts.
-# @param target String for path to hosts file.
-# @param host_entries Hash of host entries.
+# @param fqdn_entry Boolean to enable entry for fqdn.
 # @param fqdn_ip IP Address associated with entry used for FQDN.
+# @param fqdn_host_aliases String or Array of aliases for fqdn.
+# @param purge_hosts Boolean to optionally purge unmanaged entries from hosts.
+# @param host_entries Hash of host entries.
+# @param target Optional absolute path to hosts file. By default this is
+# `undef` and the provider will choose the correct path for the system.
 #
 class hosts (
-  Boolean $enable_ipv4_localhost = true,
-  Boolean $enable_ipv6_localhost = true,
-  Boolean $enable_fqdn_entry = true,
+  Boolean $fqdn_entry = true,
   Stdlib::IP::Address $fqdn_ip = $::ipaddress,
   Variant[String, Array[String, 1]] $fqdn_host_aliases = $::hostname,
-  Array[String, 1] $localhost_aliases = [
-    'localhost.localdomain',
-    'localhost4',
-    'localhost4.localdomain4',
-  ],
-  Array[String, 1] $localhost6_aliases = [
-    'localhost.localdomain',
-    'localhost',
-    'localhost6.localdomain6',
-  ],
   Boolean $purge_hosts = false,
-  Stdlib::Absolutepath $target = '/etc/hosts',
-  Variant[Undef, Hash] $host_entries = undef,
+  Optional[Hash] $host_entries = undef,
+  Optional[Stdlib::Absolutepath] $target = undef,
 ) {
 
-  if $enable_ipv4_localhost == true {
-    $localhost_ensure     = 'present'
-    $localhost_ip         = '127.0.0.1'
-    $_localhost_aliases = $localhost_aliases
-  } else {
-    $localhost_ensure     = 'absent'
-    $localhost_ip         = '127.0.0.1'
-    $_localhost_aliases = undef
-  }
-
-  if $enable_ipv6_localhost == true {
-    $localhost6_ensure     = 'present'
-    $localhost6_ip         = '::1'
-    $_localhost6_aliases = $localhost6_aliases
-  } else {
-    $localhost6_ensure     = 'absent'
-    $localhost6_ip         = '::1'
-    $_localhost6_aliases = undef
-  }
-
-  if $enable_fqdn_entry == true {
+  if $fqdn_entry == true {
     $fqdn_ensure          = 'present'
     $_fqdn_host_aliases = $fqdn_host_aliases
   } else {
@@ -64,20 +29,6 @@ class hosts (
 
   $host_defaults = {
     'target' => $target,
-  }
-
-  host { 'localhost':
-    ensure       => $localhost_ensure,
-    ip           => $localhost_ip,
-    host_aliases => $_localhost_aliases,
-    *            => $host_defaults,
-  }
-
-  host { 'localhost6':
-    ensure       => $localhost6_ensure,
-    ip           => $localhost6_ip,
-    host_aliases => $_localhost6_aliases,
-    *            => $host_defaults,
   }
 
   host { $::fqdn:
@@ -92,11 +43,16 @@ class hosts (
   }
 
   if $host_entries != undef {
-    $_host_entries = delete($host_entries,$::fqdn)
+    if $target == undef {
+      $host_entries_merged = $host_entries
+    } else {
+      $host_entries_merged = $host_defaults + $host_entries
+    }
+    $_host_entries = delete($host_entries_merged, $::fqdn)
 
-    $_host_entries.each |$k,$v| {
-      host { $k:
-        * => $v,
+    $_host_entries.each |$host, $params| {
+      host { $host:
+        * => $params,
       }
     }
   }
